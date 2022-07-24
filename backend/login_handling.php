3876a4ab711279e4c1439ce5d18c4e228231418a
Zäch <60199908+scxryshop@users.noreply.github.com>
2022-07-24 17:20:53 +0200

    core works, improvements ahead

--- backend/login_handling.php
+++ backend/login_handling.php
@@ -9,7 +9,7 @@ if(isset($_POST['login-submit'])){
 
 function BenutzerEinloggen($email, $pwd){
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
-    $sql = "SELECT Passwort FROM benutzer WHERE Email = ?";
+    $sql = "SELECT Passwort, PKBenutzer FROM benutzer WHERE Email = ?";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)){
         header("Location: ".LOGIN_ERROR_URL."error=".returnErrorMessage(7));
@@ -19,8 +19,10 @@ function BenutzerEinloggen($email, $pwd){
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $hashedpwd = "";
+    $uid = "";
     if($row = mysqli_fetch_assoc($result)){
         $hashedpwd = $row['Passwort'];
+        $uid = $row['PKBenutzer'];
     }else{
         header("Location: ".LOGIN_ERROR_URL."error=".returnErrorMessage(8));
         exit();
@@ -28,6 +30,8 @@ function BenutzerEinloggen($email, $pwd){
     mysqli_close($conn);
 
     if(password_verify($pwd,$hashedpwd)){
+        session_start();
+        $_SESSION['uid'] = $uid;
         header("Location: ../web_content/index.php");
         exit();
     }else{
--- web_content/index.php
+++ web_content/index.php
@@ -1,1 +1,17 @@
-<?php echo "logged in" ?>
+<?php session_start(); ?>
+<?php if(isset($_SESSION['uid'])): ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>You are logged in!</h1>
+    <?php else: ?>
+        <?php echo "<h1>Please login to access this page!</h1>" ?>
+    <?php endif; ?>    
+</body>
+</html>
